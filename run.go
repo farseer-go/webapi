1c8cf86ed3b5a1738ba2d97ea5f32995d768c96e
steden <1470804@qq.com>
2022-09-07 08:54:40 +0800

    先使用包裹方式

--- run.go
+++ run.go
@@ -1,5 +1,19 @@
 package webapi
 
-func Run() {
-	
+import "github.com/beego/beego/v2/server/web"
+
+// Run webapi.Run() default run on HttpPort
+// webapi.Run("localhost")
+// webapi.Run(":8089")
+// webapi.Run("127.0.0.1:8089")
+func Run(params ...string) {
+	if len(params) > 0 && params[0] != "" {
+		web.BeeApp.Run(params[0])
+	}
+	web.BeeApp.Run("")
+}
+
+// AutoRouter see HttpServer.AutoRouter
+func AutoRouter(c web.ControllerInterface) *web.HttpServer {
+	return web.BeeApp.AutoRouter(c)
 }
