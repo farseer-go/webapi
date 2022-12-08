b499a5ce9a5b7c34a313ca734de445d4b2fb62a7
steden <1470804@qq.com>
2022-12-08 20:21:23 +0800

    将webapi.RegisterXXX代码移到register.go

--- run.go
+++ run.go
@@ -2,14 +2,10 @@ package webapi
 
 import (
 	"github.com/farseer-go/fs/configure"
-	"github.com/farseer-go/fs/core/eumLogLevel"
 	"github.com/farseer-go/fs/flog"
 	"github.com/farseer-go/fs/modules"
-	"github.com/farseer-go/webapi/controller"
 	"github.com/farseer-go/webapi/middleware"
-	"github.com/farseer-go/webapi/minimal"
 	"net/http"
-	"os"
 	"strings"
 )
 
@@ -41,13 +37,6 @@ func Run(params ...string) {
 	flog.Info(http.ListenAndServe(addr, nil))
 }
 
-func RegisterMiddleware(m middleware.IMiddleware) {
-	// 需要先依赖模块
-	modules.ThrowIfNotLoad(Module{})
-
-	middleware.MiddlewareList.Add(m)
-}
-
 // Area 设置区域
 func Area(area string, f func()) {
 	if !strings.HasPrefix(area, "/") {
@@ -61,48 +50,6 @@ func Area(area string, f func()) {
 	f()
 	// 执行完后，恢复区域为"/"
 	defaultApi.area = "/"
-}
-
-// RegisterController 自动注册控制器下的所有Action方法
-func RegisterController(c controller.IController) {
-	// 需要先依赖模块
-	modules.ThrowIfNotLoad(Module{})
-
-	controller.Register(defaultApi.area, c)
-}
-
-// RegisterAction 注册单个Api
-func RegisterAction(method string, route string, actionFunc any, params ...string) {
-	// 需要先依赖模块
-	modules.ThrowIfNotLoad(Module{})
-
-	route = strings.Trim(route, " ")
-	route = strings.TrimLeft(route, "/")
-	if route == "" {
-		flog.Errorf("注册minimalApi失败：%s必须设置值", flog.Colors[eumLogLevel.Error]("route"))
-		os.Exit(1)
-	}
-	minimal.Register(defaultApi.area, method, route, actionFunc, params...)
-}
-
-// RegisterPOST 注册单个Api
-func RegisterPOST(route string, actionFunc any, params ...string) {
-	RegisterAction("POST", route, actionFunc, params...)
-}
-
-// RegisterGET 注册单个Api
-func RegisterGET(route string, actionFunc any, params ...string) {
-	RegisterAction("GET", route, actionFunc, params...)
-}
-
-// RegisterPUT 注册单个Api
-func RegisterPUT(route string, actionFunc any, params ...string) {
-	RegisterAction("PUT", route, actionFunc, params...)
-}
-
-// RegisterDELETE 注册单个Api
-func RegisterDELETE(route string, actionFunc any, params ...string) {
-	RegisterAction("DELETE", route, actionFunc, params...)
 }
 
 // UseCors 使用CORS中间件
