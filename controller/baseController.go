6e6d86c75c10fee6b70a94a8a67de9e6d309f76c
steden <1470804@qq.com>
2022-12-08 20:27:54 +0800

    重命名控制器名称

--- controller/baseController.go
+++ controller/baseController.go
@@ -2,8 +2,13 @@ package controller
 
 import (
 	"github.com/farseer-go/webapi/context"
+	"net/http"
 )
 
 type BaseController struct {
 	HttpContext context.HttpContext
+}
+
+func (receiver BaseController) init(r *http.Request) {
+
 }
--- controller/getBaseController.go
+++ controller/getBaseController.go
@@ -6,7 +6,7 @@ func getBaseController(controllerVal reflect.Value) BaseController {
 	controllerVal = reflect.Indirect(controllerVal)
 	for i := 0; i < controllerVal.NumField(); i++ {
 		fieldVal := controllerVal.Field(i)
-		if fieldVal.Type().String() == "webapi.BaseController" {
+		if fieldVal.Type().String() == "controller.BaseController" {
 			return fieldVal.Interface().(BaseController)
 		}
 	}
