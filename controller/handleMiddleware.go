3445225fd6b1059b7540a45f054732c5a7fc6e13
steden <1470804@qq.com>
2022-12-09 22:08:21 +0800

    修复controller.BaseController初始化时的错误

--- controller/handleMiddleware.go
+++ controller/handleMiddleware.go
@@ -11,14 +11,19 @@ type HandleMiddleware struct {
 func (receiver HandleMiddleware) Invoke(httpContext *context.HttpContext) {
 	// 实例化控制器
 	controllerVal := reflect.New(httpContext.Route.Controller)
-	baseController := getBaseController(controllerVal)
-
-	// 初始化
-	baseController.HttpContext = *httpContext
+	controllerElem := controllerVal.Elem()
+	// 找到 "controller.BaseController" 字段，并初始化赋值
+	for i := 0; i < controllerElem.NumField(); i++ {
+		fieldVal := controllerElem.Field(i)
+		if fieldVal.Type().String() == "controller.BaseController" {
+			fieldVal.Set(reflect.ValueOf(BaseController{HttpContext: *httpContext}))
+		}
+	}
 
 	// 入参
 	params := httpContext.GetRequestParam()
 
 	// 调用action
-	httpContext.Response.Body = controllerVal.MethodByName(httpContext.Route.ActionName).Call(params)
+	actionMethod := controllerVal.MethodByName(httpContext.Route.ActionName)
+	httpContext.Response.Body = actionMethod.Call(params)
 }
