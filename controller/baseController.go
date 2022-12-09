f1fb9f65e44acf86a8d5c742186b0800e4bd3c3e
steden <1470804@qq.com>
2022-12-09 22:09:11 +0800

    初始化Action = nil

--- controller/baseController.go
+++ controller/baseController.go
@@ -14,6 +14,9 @@ func (receiver BaseController) init(r *http.Request) {
 
 }
 
-func (receiver BaseController) getAction() map[string]Action {
+func (receiver *BaseController) getAction() map[string]Action {
+	if receiver.Action == nil {
+		receiver.Action = make(map[string]Action)
+	}
 	return receiver.Action
 }
