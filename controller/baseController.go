e8f37183a6855a56ef0d790109a86461b9d395db
steden <1470804@qq.com>
2023-01-03 15:59:03 +0800

    移除init方法

--- controller/baseController.go
+++ controller/baseController.go
@@ -2,16 +2,11 @@ package controller
 
 import (
 	"github.com/farseer-go/webapi/context"
-	"net/http"
 )
 
 type BaseController struct {
 	HttpContext context.HttpContext // 上下文
 	Action      map[string]Action   // 设置每个Action参数
-}
-
-func (receiver BaseController) init(r *http.Request) {
-
 }
 
 func (receiver *BaseController) getAction() map[string]Action {
--- controller/iController.go
+++ controller/iController.go
@@ -2,13 +2,11 @@ package controller
 
 import (
 	"github.com/farseer-go/collections"
-	"net/http"
 )
 
 // 得到IController接口的所有方法名称
 var lstControllerMethodName collections.List[string]
 
 type IController interface {
-	init(r *http.Request)         // init 初始化
 	getAction() map[string]Action // 获取Action设置
 }
