df3edbf2228c15d9a911ce2ea93dd92d8f568d79
steden <1470804@qq.com>
2023-08-14 18:52:51 +0800

    修改注释

--- route.go
+++ route.go
@@ -2,9 +2,9 @@ package webapi
 
 // Route 路由配置
 type Route struct {
-	Url     string   // 路由地址
 	Method  string   // Method类型（GET|POST|PUT|DELETE）
+	Url     string   // 路由地址
 	Action  any      // Handle
-	Params  []string // Handle的入参
 	Message string   // api返回的StatusMessage
+	Params  []string // Handle的入参
 }
