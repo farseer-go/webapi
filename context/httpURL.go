c9b75f85fefdd6dbb1ee280951b6518b89871bb6
steden <1470804@qq.com>
2023-09-21 12:59:38 +0800

    新增：获取真实IP

--- context/httpURL.go
+++ context/httpURL.go
@@ -6,14 +6,16 @@ import (
 )
 
 type HttpURL struct {
-	Url         string // 请求地址
-	Path        string // 请求地址
-	RemoteAddr  string // 客户端IP端口
-	Host        string
-	Proto       string // http协议
-	RequestURI  string
-	QueryString string
-	Query       map[string]any
+	Url             string // 请求地址
+	Path            string // 请求地址
+	RemoteAddr      string // 客户端IP端口
+	X_Forwarded_For string // 客户端IP端口
+	X_Real_Ip       string // 客户端IP端口
+	Host            string
+	Proto           string // http协议
+	RequestURI      string
+	QueryString     string
+	Query           map[string]any
 
 	R *http.Request
 }
@@ -23,4 +25,13 @@ func (r *HttpURL) parseQuery() {
 		key := strings.ToLower(k)
 		r.Query[key] = strings.Join(v, "&")
 	}
+}
+
+// GetRealIp 获取真实IP
+func (receiver *HttpURL) GetRealIp() string {
+	ip := receiver.X_Real_Ip
+	if ip == "" {
+		ip = strings.Split(receiver.X_Forwarded_For, ",")[0]
+	}
+	return strings.Split(ip, ":")[0]
 }
