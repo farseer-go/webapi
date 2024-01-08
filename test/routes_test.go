a4dd15843a853c5556ef3d0d1f26208bbbdbefca
steden <1470804@qq.com>
2024-01-08 18:53:53 +0800

    修复测试错误

--- test/routes_test.go
+++ test/routes_test.go
@@ -21,20 +21,20 @@ func TestRoutes(t *testing.T) {
 	webapi.RegisterRoutes(webapi.Route{Url: "/mini/test1", Method: "POST|GET", Action: func(req pageSizeRequest) string {
 		return fmt.Sprintf("hello world pageSize=%d，pageIndex=%d", req.PageSize, req.PageIndex)
 	}})
-	go webapi.Run(":8084")
+	go webapi.Run(":8095")
 	time.Sleep(10 * time.Millisecond)
 
-	t.Run("mini/test1:8084-POST", func(t *testing.T) {
+	t.Run("mini/test1:8095-POST", func(t *testing.T) {
 		sizeRequest := pageSizeRequest{PageSize: 10, PageIndex: 2}
 		marshal, _ := json.Marshal(sizeRequest)
-		rsp, _ := http.Post("http://127.0.0.1:8084/mini/test1", "application/json", bytes.NewReader(marshal))
+		rsp, _ := http.Post("http://127.0.0.1:8095/mini/test1", "application/json", bytes.NewReader(marshal))
 		body, _ := io.ReadAll(rsp.Body)
 		_ = rsp.Body.Close()
 		assert.Equal(t, "hello world pageSize=10，pageIndex=2", string(body))
 		assert.Equal(t, 200, rsp.StatusCode)
 	})
-	t.Run("mini/test1:8084-GET", func(t *testing.T) {
-		rsp, _ := http.Get("http://127.0.0.1:8084/mini/test1?page_size=10&PageIndex=2")
+	t.Run("mini/test1:8095-GET", func(t *testing.T) {
+		rsp, _ := http.Get("http://127.0.0.1:8095/mini/test1?page_size=10&PageIndex=2")
 		body, _ := io.ReadAll(rsp.Body)
 		_ = rsp.Body.Close()
 		assert.Equal(t, "hello world pageSize=10，pageIndex=2", string(body))
