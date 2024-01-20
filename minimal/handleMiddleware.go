cd276994c560cc584b8c4057790fcc7fdbefe432
steden <1470804@qq.com>
2024-01-20 15:59:24 +0800

    移除过滤器的埋点

--- minimal/handleMiddleware.go
+++ minimal/handleMiddleware.go
@@ -2,7 +2,6 @@ package minimal
 
 import (
 	"github.com/farseer-go/fs/container"
-	"github.com/farseer-go/fs/parse"
 	"github.com/farseer-go/fs/trace"
 	"github.com/farseer-go/webapi/context"
 	"reflect"
@@ -12,16 +11,9 @@ type HandleMiddleware struct {
 }
 
 func (receiver HandleMiddleware) Invoke(httpContext *context.HttpContext) {
-
 	// 执行过滤器OnActionExecuting
 	for i := 0; i < len(httpContext.Route.Filters); i++ {
-		traceFiltersDetail := container.Resolve[trace.IManager]().TraceHand("执行过滤器OnActionExecuting：" + parse.ToString(i+1))
 		httpContext.Route.Filters[i].OnActionExecuting(httpContext)
-		traceFiltersDetail.End(nil)
-		// 约定小于1us，不显示
-		if traceFiltersDetail.GetTraceDetail().UnTraceTs.Microseconds() <= 1 {
-			traceFiltersDetail.Ignore()
-		}
 	}
 
 	traceDetail := container.Resolve[trace.IManager]().TraceHand("执行路由")
@@ -32,12 +24,6 @@ func (receiver HandleMiddleware) Invoke(httpContext *context.HttpContext) {
 
 	// 执行过滤器OnActionExecuted
 	for i := 0; i < len(httpContext.Route.Filters); i++ {
-		traceFiltersDetail := container.Resolve[trace.IManager]().TraceHand("执行过滤器OnActionExecuted" + parse.ToString(i+1))
 		httpContext.Route.Filters[i].OnActionExecuted(httpContext)
-		traceFiltersDetail.End(nil)
-		// 约定小于1us，不显示
-		if traceFiltersDetail.GetTraceDetail().UnTraceTs.Microseconds() <= 1 {
-			traceFiltersDetail.Ignore()
-		}
 	}
 }
