5c5d9b59eac2f14e0541fca64c589e6fdcb2f597
steden <1470804@qq.com>
2024-09-10 13:04:08 +0800

    ws协议验证失败时，先主动发一条消息，然后立即关闭

--- filter/jwtFilter.go
+++ filter/jwtFilter.go
@@ -1,8 +1,10 @@
 package filter
 
 import (
+	"github.com/farseer-go/fs/core"
 	"github.com/farseer-go/fs/exception"
 	"github.com/farseer-go/webapi/context"
+	"golang.org/x/net/websocket"
 )
 
 type JwtFilter struct {
@@ -10,6 +12,10 @@ type JwtFilter struct {
 
 func (receiver JwtFilter) OnActionExecuting(httpContext *context.HttpContext) {
 	if !httpContext.Jwt.Valid() {
+		// ws协议验证失败时，先主动发一条消息，然后立即关闭
+		if httpContext.WebsocketConn != nil {
+			_ = websocket.JSON.Send(httpContext.WebsocketConn, core.ApiResponseStringError(context.InvalidMessage, context.InvalidStatusCode))
+		}
 		exception.ThrowWebExceptionf(context.InvalidStatusCode, context.InvalidMessage)
 	}
 }
